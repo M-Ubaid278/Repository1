c00f1500a00edbaf2084c55c05718de763e39635
M-Ubaid278 <ubaid89685@gmail.com>
2026-05-24 20:11:29 +0500

    changes3

--- file.cpp
+++ file.cpp
@@ -3,10 +3,10 @@ using namespace std;
 
 int main() {
     cout << "Hello World!" << endl;
-<<<<<<< HEAD
+
     cout<<"My name is Muhammad Ubaidullah:)"<<endl;
-=======
+
     cout<<"My Name is XYZ and I'm a student"<<endl;
->>>>>>> 27276d4f6d076eb9d7476dccc07b8de2b647c000
+
     return 0;
 }
